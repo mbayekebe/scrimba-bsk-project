5b203be10907048c9dc32cf97412d7d04a9c3850
Mbaye <mbayekebe@gmail.com>
2023-02-05 20:28:39 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let count1El = document.getElementById("count1-el")
+let count2El = document.getElementById("count2-el")
+let count1 = 0
+let count2 = 0
+
+function increment11() {
+    count1 += 1
+    count1El.textContent = count1
+}
+function increment12() {
+    count1 += 2
+    count1El.textContent = count1
+}
+function increment13() {
+    count1 += 3
+    count1El.textContent = count1
+}
+//======================================
+function increment21() {
+    count2 += 1
+    count2El.textContent = count2
+}
+function increment22() {
+    count2 += 2
+    count2El.textContent = count2
+}
+function increment23() {
+    count2 += 3
+    count2El.textContent = count2
+}
+
